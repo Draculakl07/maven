0405d6fff44202241a0b5ed0df352dcca9fcbeb2
Draculakl07 <avinashbabu068@gmail.com>
2023-06-10 23:34:18 +1000

    cgane

---
+++ src/main/java/com/avi/sonar/MavenSonar/Addition.java
@@ -0,0 +1,11 @@
+package com.avi.sonar.MavenSonar;
+
+public class Addition {
+    int a ;
+    int b ;
+    int c;
+
+    public void add() {
+        c = a + b;
+    }
+}
--- src/test/java/com/avi/sonar/MavenSonar/AppTest.java
+++ src/test/java/com/avi/sonar/MavenSonar/AppTest.java
@@ -1,20 +1,45 @@
 package com.avi.sonar.MavenSonar;
 
-import static org.junit.Assert.assertTrue;
-
+import org.junit.Assert;
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testAdd() {
+    	Addition addition = new Addition();
+        addition.a = 5;
+        addition.b = 10;
+        addition.add();
+        int result = addition.c;
+        Assert.assertEquals(15, result);
+
     }
+
+//    @Test
+//    public void testSubtract() {
+//        App app = new App();
+//        int result = app.subtract(5, 3);
+//        Assert.assertEquals(2, result);
+//    }
+//
+//    @Test
+//    public void testMultiply() {
+//        App app = new App();
+//        int result = app.multiply(4, 2);
+//        Assert.assertEquals(8, result);
+//    }
+//
+//    @Test
+//    public void testDivide() {
+//        App app = new App();
+//        int result = app.divide(10, 2);
+//        Assert.assertEquals(5, result);
+//    }
+//
+//    @Test(expected = IllegalArgumentException.class)
+//    public void testDivideByZero() {
+//        App app = new App();
+//        app.divide(10, 0);
+//    }
 }
